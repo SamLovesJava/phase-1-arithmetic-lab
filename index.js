ebb02a07670c4b9a3145623e641eaa7374522461
Hosam H <harooubi@gmail.com>
2023-04-07 11:20:02 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2;
+const num2 = 31
+let multiply = num1 * num2
+const random = Math.floor(Math.random(num1) * 100) + 1;
+const num3 = 14
+const num4 = 5
 
-// Write your code here
+let mod = num3 % num4
+let max = Math.max(1, 2, 20, 4, 5);
